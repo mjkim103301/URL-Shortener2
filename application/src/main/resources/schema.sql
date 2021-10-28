b87d5fb4fd43d51e9410778e97b3959ed4425eb3
Minji Kim <mjkim103301@gmail.com>
2021-10-28 23:09:40 +0900

    refactor: 스키마 파일 제거

--- application/src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-drop sequence if exists hibernate_sequence
-create sequence hibernate_sequence start with 1 increment by 1
-
-drop table url if exists
-create table url ( id int, originURL varchar(255), count int, primary key(id))
