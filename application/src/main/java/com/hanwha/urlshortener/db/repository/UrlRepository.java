67c148dacd956d91229bd62572f83edfcd9efadf
Minji Kim <mjkim103301@gmail.com>
2021-10-29 01:42:09 +0900

    refactor: 사용하지 않는 repository 메서드 제거

--- application/src/main/java/com/hanwha/urlshortener/db/repository/UrlRepository.java
+++ application/src/main/java/com/hanwha/urlshortener/db/repository/UrlRepository.java
@@ -2,10 +2,9 @@ package com.hanwha.urlshortener.db.repository;
 
 
 import com.hanwha.urlshortener.db.entity.Url;
-import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UrlRepository extends JpaRepository<Url, Long> {
 
-  Optional<Url> findByOriginalURL(String shortURL);
 }
