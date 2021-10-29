f711802053f6d6d495cff8b3179db0f089bf95ac
Minji Kim <mjkim103301@gmail.com>
2021-10-29 11:25:27 +0900

    style: 필요없는 주석 제거

--- application/src/main/java/com/hanwha/urlshortener/common/enums/ErrorType.java
+++ application/src/main/java/com/hanwha/urlshortener/common/enums/ErrorType.java
@@ -7,14 +7,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorType {
-  /* URL 단축 오류 메세지 정의 */
+
   URL_SHORTEN_NOT_FOUND(HttpStatus.NOT_FOUND, "USH_000", "해당 단축 URL에 매칭되는 URL이 존재하지 않습니다."),
   URL_FORMAT_ERROR(HttpStatus.NOT_FOUND, "USH_001", "해당 URL은 적절치 않은 형식입니다."),
-  /* URL 단축 오류 메세지 정의 끝 */
 
-  /* 기타 오류 메세지 정의 */
   ETC(HttpStatus.INTERNAL_SERVER_ERROR, "ETC_000", "알 수 없는 오류입니다.");
-  /* 기타 오류 메세지 정의 끝*/
+
 
   private final ErrorResponse errorResponse;
 
